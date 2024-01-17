1f2b3764b023482ed6e19437781d6043ddc42f69
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-17 14:46:19 +0100

    🌟 add plot to analytics to test the feature

--- fyre_app/views.py
+++ fyre_app/views.py
@@ -1,14 +1,35 @@
 from django.shortcuts import render
+import matplotlib.pyplot as plt
+import io
 
 
 def index(request):
     return render(request, "index.html")
 
+
 def analytics(request):
-    return render(request, "analytics.html")
+    plt.switch_backend('Agg')  # Switch to Agg backend
+    plt.plot(range(10))
+    plt.title("Simple Plot for demo purpose")
+    fig = plt.gcf()
+
+    fig.patch.set_facecolor('none')
+    fig.patch.set_alpha(0)
+    
+    # Save the figure to a BytesIO object
+    buf = io.BytesIO()
+    fig.savefig(buf, format='svg', transparent=True)
+    buf.seek(0)
+    
+    # Get the SVG data as a string
+    svg = buf.getvalue().decode('utf-8')
+    
+    return render(request, 'analytics.html', {'data': svg})
+
 
 def about(request):
     return render(request, "about.html")
+
 
 def contact(request):
     return render(request, "contact.html")
