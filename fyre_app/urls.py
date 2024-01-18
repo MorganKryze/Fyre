87d2d24e06ccfcdc604fc2a2a996f432c301bdb1
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-18 15:54:41 +0100

    🚧 clean url

--- fyre_app/urls.py
+++ fyre_app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views import error_500_view
 from . import views
 
 urlpatterns = [
@@ -7,5 +6,4 @@ urlpatterns = [
     path("analytics/", views.analytics, name="analytics"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
-    path('test-500/', error_500_view),
 ]
