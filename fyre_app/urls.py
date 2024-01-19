3a781c8cf3c9e8693b7b976f36581e1c0726e5a2
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-19 12:53:24 +0100

    🌟 add sections

--- fyre_app/urls.py
+++ fyre_app/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path("analytics/", views.analytics, name="analytics"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("analytics/2019/", views.analytics2019, name="analytics_2019"),
+    path("analytics/2020/", views.analytics2020, name="analytics_2020"),
+    path("analytics/2021/", views.analytics2021, name="analytics_2021"),
+    path("analytics/2022/", views.analytics2022, name="analytics_2022"),
+    path("analytics/custom/", views.analytics_custom, name="analytics_custom"),
 ]
