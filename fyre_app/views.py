c5088c5f3e45c846135a14334ce04ebf9c8adace
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-19 12:53:24 +0100

    🌟 add sections

--- fyre_app/views.py
+++ fyre_app/views.py
@@ -9,28 +9,112 @@ def index(request):
 
 
 def analytics(request):
-    plt.switch_backend('Agg')  # Switch to Agg backend
+    return render(request, "pages/analytics.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")
+
+
+def contact(request):
+    return render(request, "pages/contact.html")
+
+
+def analytics2019(request):
+    plt.switch_backend("Agg")  # Switch to Agg backend
     plt.plot(range(10))
     plt.title("Simple Plot for demo purpose")
     fig = plt.gcf()
 
-    fig.patch.set_facecolor('none')
+    fig.patch.set_facecolor("none")
     fig.patch.set_alpha(0)
-    
+
     # Save the figure to a BytesIO object
     buf = io.BytesIO()
-    fig.savefig(buf, format='svg', transparent=True)
+    fig.savefig(buf, format="svg", transparent=True)
     buf.seek(0)
-    
+
     # Get the SVG data as a string
-    svg = buf.getvalue().decode('utf-8')
-    
-    return render(request, 'analytics.html', {'data': svg})
+    svg = buf.getvalue().decode("utf-8")
+
+    return render(request, "pages/analytics/2019.html", {"data": svg})
 
 
-def about(request):
-    return render(request, "about.html")
+def analytics2020(request):
+    plt.switch_backend("Agg")  # Switch to Agg backend
+    plt.plot(range(10))
+    plt.title("Simple Plot for demo purpose")
+    fig = plt.gcf()
+
+    fig.patch.set_facecolor("none")
+    fig.patch.set_alpha(0)
+
+    # Save the figure to a BytesIO object
+    buf = io.BytesIO()
+    fig.savefig(buf, format="svg", transparent=True)
+    buf.seek(0)
+
+    # Get the SVG data as a string
+    svg = buf.getvalue().decode("utf-8")
+
+    return render(request, "pages/analytics/2020.html", {"data": svg})
 
 
-def contact(request):
-    return render(request, "contact.html")
+def analytics2021(request):
+    plt.switch_backend("Agg")  # Switch to Agg backend
+    plt.plot(range(10))
+    plt.title("Simple Plot for demo purpose")
+    fig = plt.gcf()
+
+    fig.patch.set_facecolor("none")
+    fig.patch.set_alpha(0)
+
+    # Save the figure to a BytesIO object
+    buf = io.BytesIO()
+    fig.savefig(buf, format="svg", transparent=True)
+    buf.seek(0)
+
+    # Get the SVG data as a string
+    svg = buf.getvalue().decode("utf-8")
+
+    return render(request, "pages/analytics/2021.html", {"data": svg})
+
+
+def analytics2022(request):
+    plt.switch_backend("Agg")  # Switch to Agg backend
+    plt.plot(range(10))
+    plt.title("Simple Plot for demo purpose")
+    fig = plt.gcf()
+
+    fig.patch.set_facecolor("none")
+    fig.patch.set_alpha(0)
+
+    # Save the figure to a BytesIO object
+    buf = io.BytesIO()
+    fig.savefig(buf, format="svg", transparent=True)
+    buf.seek(0)
+
+    # Get the SVG data as a string
+    svg = buf.getvalue().decode("utf-8")
+
+    return render(request, "pages/analytics/2022.html", {"data": svg})
+
+
+def analytics_custom(request):
+    plt.switch_backend("Agg")  # Switch to Agg backend
+    plt.plot(range(10))
+    plt.title("Simple Plot for demo purpose")
+    fig = plt.gcf()
+
+    fig.patch.set_facecolor("none")
+    fig.patch.set_alpha(0)
+
+    # Save the figure to a BytesIO object
+    buf = io.BytesIO()
+    fig.savefig(buf, format="svg", transparent=True)
+    buf.seek(0)
+
+    # Get the SVG data as a string
+    svg = buf.getvalue().decode("utf-8")
+
+    return render(request, "pages/analytics/custom.html", {"data": svg})
