e8e8b9ecb61361ff9239ac9cc763d3db003819e9
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-01-24 20:37:34 +0100

    🌟 add plotly instead of matplotly

--- fyre_app/views.py
+++ fyre_app/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-import matplotlib.pyplot as plt
-import io
 # from django.utils.translation import gettext as _
 
 
@@ -33,80 +31,16 @@ def analytics2019(request):
 
 
 def analytics2020(request):
-    plt.switch_backend("Agg")  # Switch to Agg backend
-    plt.plot(range(10))
-    plt.title("Simple Plot for demo purpose")
-    fig = plt.gcf()
-
-    fig.patch.set_facecolor("none")
-    fig.patch.set_alpha(0)
-
-    # Save the figure to a BytesIO object
-    buf = io.BytesIO()
-    fig.savefig(buf, format="svg", transparent=True)
-    buf.seek(0)
-
-    # Get the SVG data as a string
-    svg = buf.getvalue().decode("utf-8")
-
-    return render(request, "pages/analytics/2020.html", {"data": svg})
+    return render(request, "pages/analytics/2020.html")
 
 
 def analytics2021(request):
-    plt.switch_backend("Agg")  # Switch to Agg backend
-    plt.plot(range(10))
-    plt.title("Simple Plot for demo purpose")
-    fig = plt.gcf()
-
-    fig.patch.set_facecolor("none")
-    fig.patch.set_alpha(0)
-
-    # Save the figure to a BytesIO object
-    buf = io.BytesIO()
-    fig.savefig(buf, format="svg", transparent=True)
-    buf.seek(0)
-
-    # Get the SVG data as a string
-    svg = buf.getvalue().decode("utf-8")
-
-    return render(request, "pages/analytics/2021.html", {"data": svg})
+    return render(request, "pages/analytics/2021.html")
 
 
 def analytics2022(request):
-    plt.switch_backend("Agg")  # Switch to Agg backend
-    plt.plot(range(10))
-    plt.title("Simple Plot for demo purpose")
-    fig = plt.gcf()
-
-    fig.patch.set_facecolor("none")
-    fig.patch.set_alpha(0)
-
-    # Save the figure to a BytesIO object
-    buf = io.BytesIO()
-    fig.savefig(buf, format="svg", transparent=True)
-    buf.seek(0)
-
-    # Get the SVG data as a string
-    svg = buf.getvalue().decode("utf-8")
-
-    return render(request, "pages/analytics/2022.html", {"data": svg})
+    return render(request, "pages/analytics/2022.html")
 
 
 def analytics_custom(request):
-    plt.switch_backend("Agg")  # Switch to Agg backend
-    plt.plot(range(10))
-    plt.title("Simple Plot for demo purpose")
-    fig = plt.gcf()
-
-    fig.patch.set_facecolor("none")
-    fig.patch.set_alpha(0)
-
-    # Save the figure to a BytesIO object
-    buf = io.BytesIO()
-    fig.savefig(buf, format="svg", transparent=True)
-    buf.seek(0)
-
-    # Get the SVG data as a string
-    svg = buf.getvalue().decode("utf-8")
-
-    return render(request, "pages/analytics/custom.html", {"data": svg})
+    return render(request, "pages/analytics/custom.html")
